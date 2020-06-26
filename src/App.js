8d4776d70ac35927698b01752d9c66df0008a2d6
irina <irisha.karpova@gmail.com>
2020-06-26 11:14:48 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import desktopVideo from './video/video_desc.mov';
+import mobileVideo from './video/video_mobile.mp4'
+import poster from './img/image_bg.jpg'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default ()=> {
 
-export default App;
+  const [isLoaded, setIsLoaded ]  = useState(false)
+  const [scrWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+        setIsLoaded(true)
+    })
+  })
+
+  const getVideo = (scrWidth) => {
+    if(scrWidth >= 768) return desktopVideo
+    return mobileVideo
+  }
+
+
+  return(
+    <div className='video-wrap'
+         style={{opacity: isLoaded ? 1 : 0, transition: 'opacity 2s ease'}}>
+
+      {isLoaded ? (
+        <video
+          muted
+          playsInline
+          autoPlay
+          loop
+          poster={poster}
+          className='video-content'>
+         <source src={ getVideo(scrWidth) } type="video/mp4" />
+         </video>
+      ):
+        <img src={poster}  alt='video background'/> }
+    </div>
+  )
+}
